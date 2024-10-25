c2169e71c383ea0a5fad64262e8a14b8164a0fd6
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-10-25 17:24:17 +0200

    fix: add on scroll events prevention

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import ScrollDot from '$lib/components/ScrollDot.svelte';
 </script>
 
-<div class="w-full h-screen flex items-center justify-center">
+<div
+	class="w-full h-screen flex items-center justify-center overscroll-none"
+	onscroll={(scroll) => scroll.preventDefault()}
+>
 	<ScrollDot />
 </div>
